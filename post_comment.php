64e80dd3d7358f9a8ce839d5eca27a657af061a7
Isabella Benavente <ibenaven@usc.edu>
2014-11-08 14:42:34 -0800

    fixed the media carousel

--- post_comment.php
+++ post_comment.php
@@ -1,10 +1,35 @@
 <?php
-    require('Persistence.php');
 
-    $db = new Persistence();
-    if( $db->add_comment($_POST) ) {
-      header( 'Location: index.php' );
-    }
-    else {
-      header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
-    }
+require('Persistence.php');
+
+$ajax = ($_SERVER[ 'HTTP_X_REQUESTED_WITH' ] === 'XMLHttpRequest');
+
+$db = new Persistence();
+$added = $db->add_comment($_POST);
+
+if($ajax) {
+  sendAjaxResponse($added);
+}
+else {
+  sendStandardResponse($added); 
+}
+
+function sendAjaxResponse($added) {
+  header("Content-Type: application/x-javascript");
+  if($added) {
+    header( 'Status: 201' );
+    echo( json_encode($added) );
+  }
+  else {
+    header( 'Status: 400' );
+  }
+}
+
+function sendStandardResponse($added) {
+  if($added) {
+    header( 'Location: index.php' );
+  }
+  else {
+    header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  }
+}
